e70404e8c67dfd2765dddab4c6f2208d3e80aae2
shubham-kanodia <shubhamkanodia123@gmail.com>
2021-09-04 02:51:51 +0530

    Adding hardhat config

---
+++ hardhat.config.ts
@@ -0,0 +1,54 @@
+import '@typechain/hardhat'
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-waffle'
+import '@nomiclabs/hardhat-etherscan'
+import 'hardhat-contract-sizer'
+import 'solidity-coverage'
+
+import { HardhatUserConfig } from 'hardhat/config'
+import { SolcUserConfig } from 'hardhat/types'
+
+const DEFAULT_COMPILER_SETTINGS: SolcUserConfig = {
+  version: '0.8.0'
+}
+
+const config: HardhatUserConfig = {
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: false,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+  },
+  solidity: {
+    compilers: [DEFAULT_COMPILER_SETTINGS],
+  },
+  contractSizer: {
+    alphaSort: false,
+    disambiguatePaths: false,
+    runOnCompile: true,
+  },
+}
+
+if (process.env.ETHERSCAN_API_KEY) {
+  config.etherscan = {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  }
+}
+
+export default config
