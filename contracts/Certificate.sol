6cce046e585ace2632b9fb4941f6c99125073906
shubham-kanodia <shubhamkanodia123@gmail.com>
2021-09-05 00:04:41 +0530

    Adding lint check

--- contracts/Certificate.sol
+++ contracts/Certificate.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
 
 contract Certificate is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("Certificate", "CERT") {}
+    constructor() ERC721('Certificate', 'CERT') {}
 
     function awardCertificate(address player, string memory tokenURI)
         public
@@ -22,12 +22,12 @@ contract Certificate is ERC721URIStorage {
 
         return newCertificateId;
     }
-    
-    modifier disallowTransfer {
-        require(false, "Token transfer is not allowed");
+
+    modifier disallowTransfer() {
+        require(false, 'Token transfer is not allowed');
         _;
     }
-    
+
     function transferFrom(
         address from,
         address to,
@@ -35,7 +35,7 @@ contract Certificate is ERC721URIStorage {
     ) public virtual override disallowTransfer {
         super.transferFrom(from, to, tokenId);
     }
-    
+
     function safeTransferFrom(
         address from,
         address to,
@@ -43,7 +43,7 @@ contract Certificate is ERC721URIStorage {
     ) public virtual override disallowTransfer {
         super.safeTransferFrom(from, to, tokenId);
     }
-    
+
     function safeTransferFrom(
         address from,
         address to,
