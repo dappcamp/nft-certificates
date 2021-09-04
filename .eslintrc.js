fb6036861dfb3e0db83f672c96696453fb3f39d7
shubham-kanodia <shubhamkanodia123@gmail.com>
2021-09-05 00:42:17 +0530

    Configuring eslint + adding scripts

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+      '@typescript-eslint',
+      'unused-imports',
+      'promise'
+    ],
+    extends: [
+      'prettier'
+    ],
+    rules: {
+      "@typescript-eslint/no-unused-vars": 1,
+      "unused-imports/no-unused-imports-ts": 1,
+      "no-shadow": 1
+    }
+};
