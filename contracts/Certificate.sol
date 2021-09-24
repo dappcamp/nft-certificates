6027453bd16a7dda6a4373ad4e57dc49a4e9004f
shubham-kanodia <shubhamkanodia123@gmail.com>
2021-09-24 18:31:39 +0530

    Making certificate contract ownable

--- contracts/Certificate.sol
+++ contracts/Certificate.sol
@@ -3,21 +3,23 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Certificate is ERC721URIStorage {
+contract Certificate is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor() ERC721("Certificate", "CERT") {}
 
-    function awardCertificate(address player, string memory tokenURI)
+    function awardCertificate(address to, string memory tokenURI)
         public
+        onlyOwner
         returns (uint256)
     {
         _tokenIds.increment();
 
         uint256 newCertificateId = _tokenIds.current();
-        _mint(player, newCertificateId);
+        _mint(to, newCertificateId);
         _setTokenURI(newCertificateId, tokenURI);
 
         return newCertificateId;
