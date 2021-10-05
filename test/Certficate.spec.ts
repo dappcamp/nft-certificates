3f54d07d219f7f6ce9f7bb6cdcf5072a7095a933
Aniket <aniketguptaknp@gmail.com>
2021-10-05 17:03:14 +0530

    Add non transferable test

--- test/Certficate.spec.ts
+++ test/Certficate.spec.ts
@@ -8,10 +8,10 @@ use(solidity)
 
 describe('Certficates', () => {
     let certificate: Certificate
-    let owner: Wallet, player1: Wallet
+    let owner: Wallet, player1: Wallet, player2: Wallet
 
     beforeEach('deploy Certificate', async() => {
-        [owner, player1] = await (ethers as any).getSigners();
+        [owner, player1, player2] = await (ethers as any).getSigners();
         const factory = await ethers.getContractFactory('Certificate')
         certificate = (await factory.deploy()) as Certificate
     })
@@ -19,6 +19,13 @@ describe('Certficates', () => {
     describe("award certificate", () => {
         it("should not be mintable by anyone except the owner", async() => {
             await expect(certificate.connect(player1).awardCertificate(player1.address, "tokenURI")).to.be.revertedWith('Ownable: caller is not the owner')
+        })
+
+        it("should not be transferable after the first transfer", async() => {
+            const newTokenURI = await certificate.connect(owner).callStatic.awardCertificate(player1.address,
+            "tokenURI");
+            await expect(certificate.connect(player1).transferFrom(player1.address, player2.address, 
+            newTokenURI)).to.be.revertedWith('Token transfer is not allowed');
         })
     })
 
