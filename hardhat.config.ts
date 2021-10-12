40630fa1932229e66486dc9c6203ae28479857c0
shubham-kanodia <shubhamkanodia123@gmail.com>
2021-10-13 01:28:47 +0530

    Fixing failing pipeline

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -29,7 +29,7 @@ const config: HardhatUserConfig = {
     },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [`0x${process.env.WALLET_PRIVATE_KEY}`],
+      // accounts: [`0x${process.env.WALLET_PRIVATE_KEY}`],
     },
     goerli: {
       url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
