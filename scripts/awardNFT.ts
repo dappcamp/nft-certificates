ed1053e07fa82c2d64170306d88b32e86e61d8d8
shubham-kanodia <shubhamkanodia123@gmail.com>
2021-10-22 20:33:24 +0530

    Modifying deployment scripts

--- scripts/awardNFT.ts
+++ scripts/awardNFT.ts
@@ -3,45 +3,47 @@ import * as fs from "fs";
 import { Contract } from "@ethersproject/contracts";
 
 async function awardCertificate(
-  certificate: Contract,
-  toAddress: string,
-  metadataURI: string
+    dAppCampCertificate: Contract,
+    toAddress: string,
+    metadataURI: string
 ) {
-  const txn = await certificate.awardCertificate(toAddress, metadataURI);
-  return await txn.wait();
+    const txn = await dAppCampCertificate.awardCertificate(
+        toAddress,
+        metadataURI
+    );
+    return await txn.wait();
 }
 
 async function main() {
-
-  //fetch certificate
-  const addressData = JSON.parse(
-    fs.readFileSync("metadata/deployedAddress.json","utf-8")
-  ); 
-
-  const address = addressData["address"];
-  const certContract = await ethers.getContractFactory('Certificate');
-  const certificate = certContract.attach(address);
-
-  const data = JSON.parse(
-    fs.readFileSync("metadata/storeScriptOp.json", "utf-8")
-  );
-
-  for (let elem of data) {
-    const receipt = await awardCertificate(
-      certificate,
-      elem.address,
-      elem.metadataURI
+    //fetch certificate
+    const addressData = JSON.parse(
+        fs.readFileSync("metadata/deployedAddress.json", "utf-8")
     );
 
-    if (receipt.events.pop().event !== "Transfer") {
-      process.exit(1);
+    const address = addressData["address"];
+    const certContract = await ethers.getContractFactory("DappCampCertificate");
+    const certificate = certContract.attach(address);
+
+    const data = JSON.parse(
+        fs.readFileSync("metadata/storeScriptOp.json", "utf-8")
+    );
+
+    for (let elem of data) {
+        const receipt = await awardCertificate(
+            certificate,
+            elem.address,
+            elem.metadataURI
+        );
+
+        if (receipt.events.pop().event !== "Transfer") {
+            process.exit(1);
+        }
     }
-  }
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.log(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error);
+        process.exit(1);
+    });
